eac8549199d4d2b026323a1ce2ff629d1b785f1c
CJ <cjzhang88@gmail.com>
2017-03-01 14:30:17 +0800

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
 
 	def create
-	  param! :name, String, required: true
-	  param! :url, String, required: true
+	  # param! :name, String, required: true
+	  # param! :url, String, required: true
 
-	  name = params[:name]
-	  avatar = params[:url]
+	  # name = params[:name]
+	  # avatar = params[:url]
 
-      @user = User.new(name: name, avatar: avatar)
+   #    @user = User.new(name: name, avatar: avatar)
 
-	  if !@user.save
-        return render json: {error: 4000, msg: '创建用户失败'}, status: 400
-	  end
+	  # if !@user.save
+   #      return render json: {error: 4000, msg: '创建用户失败'}, status: 400
+	  # end
 
+       @user = User.new(name: '', avatar: '')
 	end
 
 	def show
