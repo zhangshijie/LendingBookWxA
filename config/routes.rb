c3ab14a022c696a1e0c18561a329855428ceefbb
CJ <cjzhang88@gmail.com>
2017-03-01 13:19:56 +0800

    添加路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'book/user'
+
+	resources :users
+	resources :books
+  # get 'book/user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
