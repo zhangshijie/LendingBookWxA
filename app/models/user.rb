9a5119f4c580356ce7dec5ea18c7f860f8b9f36d
CJ <cjzhang88@gmail.com>
2017-03-01 18:34:34 +0800

    种子用户

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  has_secure_token :auth_token
+  # has_secure_token :auth_token
+
+  def create
+     @user = User.new("name": params[:name],"avatar": params[:avatar])
+     @user.save
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+2.times do |i|
+   User.create(name:"UserName ##{i}}",avatar:"http://www.baidu.com")
+end
